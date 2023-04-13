0551cf61853e84c34c4bc86676f216dacadacdaa
Emanuel Silva <emanuel.silva@grupofm.local>
2023-04-13 11:50:50 -0300

    adicionar as telas de Balance e Moviments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Moviments from '../../components/Moviments';
+
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto equatorial',
+    value: '65,23',
+    date: '09/04/2023',
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: 'Pix YTALO AMORIN',
+    value: '150,00',
+    date: '13/04/2023',
+    type: 1 //receitas
+  },
+  {
+    id: 3,
+    label: 'OG1 Salário',
+    value: '33,50',
+    date: '14/04/2023',
+    type: 1 //despesas
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Emanoel Silva"/>
-      <Text>Página Home!</Text>
-      {/* <StatusBar style="auto" /> */}
+      <Balance saldo="3.500,00" gastos="150,00"/>
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={ (item) => String(item.id) }
+        showsVerticalScrollIndicator={false}
+        renderItem={ ({item}) => <Moviments data={item}/>}
+      />
     </View>
   );
 }
@@ -15,8 +50,17 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fafafa',
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14
+  }
 });
